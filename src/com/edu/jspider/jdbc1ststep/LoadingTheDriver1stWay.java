06e6b07717a31d3bd3701f0582836cd7a759582c
arsAravind <saravindan7871@gmail.com>
2024-02-25 22:28:46 +0530

    Merge branch 'master' of https://github.com/arsAravind/JDBC.git

--- src/com/edu/jspider/jdbc1ststep/LoadingTheDriver1stWay.java
+++ src/com/edu/jspider/jdbc1ststep/LoadingTheDriver1stWay.java
@@ -1,5 +1,6 @@
 package com.edu.jspider.jdbc1ststep;
 //API is the collection of package which help us to communicate or interact b/w 2 app
+
 //JDBC is api used to communicate b/w the java app and RDBMS(MYsql) application
 //to make use of JDBC we have to load the Driver(MySQL)
 //Driver's(MYSQL driver) are present in form of .jar file which is not having in java libraries
@@ -9,19 +10,16 @@ package com.edu.jspider.jdbc1ststep;
 //now MySQL driver c is ready and then we have to store MySQLdriver in refvar(Driver c of java.sql.Driver)..upcasting
 //And by registerDriver(driver//i.e refvar)method and finally we can load the driver(MySQL driver)
 
-
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class LoadingTheDriver1stWay {
 	public static void main(String[] args) {
-		try 
-		{			//driver are give it over the (driver) refvariable
-			java.sql.Driver driver =new com.mysql.jdbc.Driver();//mySql driver package
+		try { // driver are give it over the (driver) refvariable
+			java.sql.Driver driver = new com.mysql.jdbc.Driver();// mySql driver package
 			DriverManager.registerDriver(driver);
 			System.out.println("loading the driver successfully done..");
-		} 
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		System.out.println("hi");
